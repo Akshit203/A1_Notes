c0b8ba74a869bdd8bc62c37d9f32c00358a60849
Akshit203 <akshitgupta2003.ak@gmail.com>
2024-07-20 18:48:50 +0530

    script.js

---
+++ demo.js
@@ -0,0 +1,15 @@
+const addbuton = document.querySelector("#add")
+
+const AddNewNote = () =>{
+
+    const note = document.createElement(div);
+    note.classList.add("notes")
+
+    const addHtml = `<div> chandigarh </div>`
+    
+
+
+
+}
+
+addbuton.addEventListener("onclick", AddNewNote());
--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-const addButton = document.querySelector("#add");
+document.addEventListener("DOMContent", () => {
 
-const addNewNote = (text = "") => {
-    const note = document.createElement("div");
-    note.classList.add("notes");
+    const addButton = document.querySelector("#add");
   
-    const new_notes = `
+    const updateLSData = () => {
 
-         <div class="operation">
+      const textAreaData = document.querySelectorAll("textarea");
+      const notes = [];
+      textAreaData.forEach((note) => notes.push(note.value));
+      localStorage.setItem("notes", JSON.stringify(notes));
 
-            <button class="edit">
-                <img class="icon-add" width="24" height="24" src="https://img.icons8.com/ios-filled/50/edit-text-file.png" alt="edit-text-file"/> 
-            </button>
+    };
+  
+    const addNewNote = (text = "") => {
 
-            <button class="delete">
-                 <!-- <i class="fas fa-trash-alt"></i>  -->
-                <img class="icon-del" width="28" height="28" src="https://img.icons8.com/color/48/delete-property.png" alt="delete-property"/>
-            </button>
-
+      const note = document.createElement("div");
+      note.classList.add("notes");
+  
+      const new_notes = `
+        <div class="operation">
+          <button class="edit">
+            <img class="icon-add" width="24" height="24" src="https://img.icons8.com/ios-filled/50/edit-text-file.png" alt="edit-text-file"/> 
+          </button>
+          <button class="delete">
+            <img class="icon-del" width="28" height="28" src="https://img.icons8.com/color/48/delete-property.png" alt="delete-property"/>
+          </button>
         </div> 
-
-        <div class= "main" > </div>
-        <textarea class= ""> </textarea> `;
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>`;
   
-    note.insertAdjacentHTML("afterbegin", new_notes);
-
-    document.body.appendChild(note)
-
-}
-
-addButton.addEventListener('click', () => addNewNote());
-
-
-
+      note.insertAdjacentHTML("afterbegin", new_notes);
+  
+      document.body.appendChild(note);
+  
+      const editButton = note.querySelector(".edit");
+      const delButton = note.querySelector(".delete");
+      const mainDiv = note.querySelector(".main");
+      const textArea = note.querySelector("textarea");
+  
+      textArea.value = text;
+      mainDiv.innerHTML = text;
+  
+      delButton.addEventListener("click", () => {
+        note.remove();
+        updateLSData();
+      });
+  
+      editButton.addEventListener("click", () => {
+        mainDiv.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+      });
+  
+      textArea.addEventListener("change", (event) => {
+        const value = event.target.value;
+        mainDiv.innerHTML = value;
+        updateLSData();
+      });
+    };
+  
+    // Load existing notes from localStorage
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    
+    if (notes) {
+      notes.forEach((note) => addNewNote(note));
+    }
+  
+    addButton.addEventListener("click", () => addNewNote());
+  });
+  
